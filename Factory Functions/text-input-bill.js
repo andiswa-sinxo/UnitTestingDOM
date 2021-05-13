cdabd0a409398be6bb8b11a13e7f15ac8af3f813
Andiswa <andiswasinxo@gmail.com>
2021-05-13 16:02:27 +0200

    text input

--- Factory Functions/text-input-bill.js
+++ Factory Functions/text-input-bill.js
@@ -1,13 +1,48 @@
 function textInputBill(){
-    var theCall = 0;
-    var theSms = 0;
+    var textTotal = 0;
+    var callsTotal = 0;
+    var smsTotal = 0;
 
-    var theCallTotal = 0;
-    var theSmsTotal =0;
 
-    function enterCall(callAmount){
-        
+    function textBillTotal(textString){
+        // get the value entered in the billType textfield
+        var billTypeEntered = textString.trim();
+        // update the correct total
+        if (billTypeEntered === "call"){
+            callsTotal += 2.75
+        }
+        else if (billTypeEntered === "sms"){
+            smsTotal += 0.75;
+        }
     }
 
-}
+    function getTotalText(){
+        return textTotal;
 
+    }
+
+    function getCallTotal(){
+        return callsTotal;
+    }
+
+    function getSmsTotal(){
+        return smsTotal;
+    }
+
+    function warningCriticalLevel(){
+        if (getTotalText() >= 30 && getTotalText() < 50){
+            return 'warning'
+        }
+        else if (getTotalText() >= 50){
+            return 'critical'
+        }
+    }
+
+    return{
+        textBillTotal,
+        getTotalText,
+        getCallTotal,
+        getSmsTotal,
+        warningCriticalLevel
+}
+}
--- Test/text-input-bill-test.js
+++ Test/text-input-bill-test.js
@@ -1,6 +1,11 @@
 describe("The Text input bill factory function" , function() {
     describe("enter value" , function() {
-        it ("should be to enter the call cost in the textbox" , function(){
+        it ("should be able to enter the call cost in the textbox" , function(){
+            let textInput = textInputBill();
+
+            textInput.textBillTotal('call');
+            assert.equal(2.75, textInput.getCallTotal());
+            assert.equal(2.75, textInput.getTotalText());
     });
 });
 });
